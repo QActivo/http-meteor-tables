af645b294ddf5bdc9ab3c907c8085fe4eaadf311
Vercryger <abuzaid.karim@gmail.com>
2017-09-15 20:18:32 -0300

    fix search_fields query

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'qactivo:http-meteor-tables',
   summary: 'Datatables for large or small datasets in Meteor',
   git: 'https://github.com/QActivo/http-meteor-tables.git',
-  version: '1.0.3'
+  version: '1.0.4'
 });
 
 Package.onUse(function (api) {
